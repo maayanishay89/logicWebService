c4e1eb85b67fc17e548f5a1d5a77cbb73aca9227
maayanishay89 <maayanishay@gmail.com>
2016-05-17 23:22:45 +0300

    controller
    controller

--- controller/controller.js
+++ controller/controller.js
@@ -2,28 +2,28 @@ var utils = require("./../model/utils/utils");
 var validation = require("./../model/utils/validation");
 var Matcher = require("../model/logic/matcher"); 
 
-function calculateMatching(req,res){ 
-
-   if (validation.addMatchingObject(req)) {
-   //	console.log("im in if");
-		var formula = JSON.stringify(req.body);
-	  	Matcher.calculateMatching(formula, function(result) {
-            res.status(formula).json(result);
-        });
-    } else {
-    	    	console.log("im in else");
-        utils.sendErrorValidation(res);
-    }
-
-};
-
 // function calculateMatching(req,res){ 
-//     //console.log("im innn");
-//     var formula = JSON.stringify(req.body);
-//     Matcher.calculateMatching(formula, function(result) {
-//         res.json(result);   
-//     });
+
+//    if (validation.addMatchingObject(req)) {
+//    //	console.log("im in if");
+// 		var formula = JSON.stringify(req.body);
+// 	  	Matcher.calculateMatching(formula, function(result) {
+//             res.status(formula).json(result);
+//         });
+//     } else {
+//     	    	console.log("im in else");
+//         utils.sendErrorValidation(res);
+//     }
+
 // };
+
+function calculateMatching(req,res){ 
+    //console.log("im innn");
+    var formula = JSON.stringify(req.body);
+    Matcher.calculateMatching(formula, function(result) {
+        res.json(result);   
+    });
+};
 
 exports.calculateMatching = calculateMatching;
 
